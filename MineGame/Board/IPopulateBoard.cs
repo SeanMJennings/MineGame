893d5e601c22577fd78c7d2c3a2fef43c976a6a0
CuriousConsistency <seanjennings976@gmail.com>
2023-05-14 21:38:51 +0100

    Removed first attempt

--- MineGame/Board/IPopulateBoard.cs
+++
@@ -1,8 +0,0 @@
-using MineGame.Domain;
-
-namespace MineGame.Board;
-
-public interface IPopulateBoard
-{
-    public List<Landmine> PopulateLandmines(int row, int columns, int numberOfLandmines);
-}
