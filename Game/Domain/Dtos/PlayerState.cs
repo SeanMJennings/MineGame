5f3da2986be18f29dd10333b25b3456483621783
CuriousConsistency <seanjennings976@gmail.com>
2024-03-21 18:05:59 +0000

    refactor

--- Game/Domain/Dtos/PlayerState.cs
+++ Game/Domain/Dtos/PlayerState.cs
@@ -2,9 +2,4 @@ namespace Game.Domain.Dtos;
 
 using Primitives;
 
-public class PlayerState(Position position, int landminesHit)
-{
-    public Position Position { get; } = position;
-
-    public int LandminesHit { get; } = landminesHit;
-}
+public record PlayerState(Position Position, int LandminesHit);
